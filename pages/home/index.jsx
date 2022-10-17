4ec2cff39046329ea83f87bef2a9dc1a753defbf
ashikmahmud <ashikmamud187@gmail.com>
2022-10-17 23:24:32 +0600

    yah done to fixes somne issues and optimze height

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -1,8 +1,11 @@
+import Head from "next/head";
 import React from "react";
-
 const HomeDefault = () => {
   return (
     <>
+      <Head>
+        <title>Home Page</title>
+      </Head>
       <div>
         <div className="container mx-auto p-4">
           <h3 className="text-3xl">Home Page</h3>
